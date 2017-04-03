054f3232cf08500c01e9f0e037887a97a895c6b9
Josh <josh@debian>
2017-04-03 16:43:10 +0100

    Made a eval string for evaluating statements of parsed strings

--- WhileParsec/WhileParser.hs
+++ WhileParsec/WhileParser.hs
@@ -23,39 +23,28 @@ type DecV = [(Var,Aexp)]
 type DecP = [(Pname,Stm)]
 
 
+data Aexp = N Num 
+          | V Var 
+          | Mult Aexp Aexp
+          | Add Aexp Aexp
+          | Sub Aexp Aexp
+        deriving (Show)
 
-data Stm = Seq [Stm]
-         | Skip
+data Bexp = TRUE
+          | FALSE
+          | Neg Bexp
+          | And Bexp Bexp
+          | Eq Aexp Aexp
+          | Le Aexp Aexp
+        deriving (Show)
+
+data Stm = Skip
          | Ass Var Aexp 
          | Comp Stm Stm 
          | If Bexp Stm Stm
          | While Bexp Stm
          | Block DecV DecP Stm
          | Call Pname
-        deriving (Show)
-
-data Bexp = TRUE
-          | FALSE
-          | Neg Bexp
-          | BBinary BBinOp Bexp Bexp
-          | RBinary RBinOp Aexp Aexp
-        deriving (Show)
-
-data BBinOp = And
-        deriving (Show)
-
-data RBinOp = Eq
-            | Le
-        deriving (Show)
-
-data Aexp = N Num 
-          | V Var 
-          | ABinary ABinOp Aexp Aexp
-        deriving (Show)
-
-data ABinOp = Mult
-            | Add
-            | Sub
         deriving (Show)
 
 languageDef =
@@ -108,11 +97,6 @@ statement =   parens statement
          <|>  compStm
          <|>  statement'
 
-compStm :: Parser Stm
-compStm = buildExpressionParser compOp statement'
-
-compOp = [[Infix  (reservedOp ";"   >> return (Comp )) AssocLeft]]
-
 statement' :: Parser Stm
 statement' =  ifStm
           <|> whileStm
@@ -148,6 +132,11 @@ assignStm =
 
 skipStm :: Parser Stm
 skipStm = reserved "skip" >> return Skip
+
+compStm :: Parser Stm
+compStm = buildExpressionParser compOp statement'
+
+compOp = [[Infix  (reservedOp ";"   >> return (Comp )) AssocLeft]]
 
 callStm :: Parser Stm
 callStm = 
@@ -191,13 +180,13 @@ bexp = buildExpressionParser bOp bTerm
 pname :: Parser Pname
 pname = identifier
 
-aOp = [        [Infix  (reservedOp "*"   >> return (ABinary Mult)) AssocLeft ]
-             , [Infix  (reservedOp "+"   >> return (ABinary Add     )) AssocLeft,
-                Infix  (reservedOp "-"   >> return (ABinary Sub)) AssocLeft]
+aOp = [        [Infix  (reservedOp "*"   >> return (Mult)) AssocLeft ]
+             , [Infix  (reservedOp "+"   >> return (Add     )) AssocLeft,
+                Infix  (reservedOp "-"   >> return (Sub)) AssocLeft]
               ]
 
 bOp = [ [Prefix (reservedOp "!" >> return (Neg             ))          ]
-             , [Infix  (reservedOp "and" >> return (BBinary And     )) AssocLeft]
+             , [Infix  (reservedOp "and" >> return (And     )) AssocLeft]
              ]
 
 aTerm =  parens aexp
@@ -213,7 +202,7 @@ rExpression =
   do a1 <- aexp
      op <- relation
      a2 <- aexp
-     return $ RBinary op a1 a2
+     return $ op a1 a2
 
 relation = (reservedOp "<" >> return Le)
         <|> (reservedOp "=" >> return Eq)
@@ -231,27 +220,18 @@ parseFile file =
        Left e  -> print e >> fail "parse error"
        Right r -> return r
 
-
-evalString :: String -> String -> StateS
-evalString str v = 
-  do stm <- parseString
-     s_ds stm s
+evalString :: String -> StateS -> StateS
+evalString str s = s_ds (parseString str) s
 
 n_val :: Num -> Z
 n_val x = x 
 
-s :: StateS
-s "x" = n_val 5
-s "y" = n_val 2
-s "z" = n_val 3
-s var = n_val 0
-
 a_val :: Aexp -> StateS -> Z
 a_val (N n) s = n_val n
 a_val (V v) s = s v
-a_val (ABinary Mult a1 a2) s = (a_val a1 s) * (a_val a2 s)
-a_val (ABinary Add a1 a2) s = (a_val a1 s) + (a_val a2 s)
-a_val (ABinary Sub a1 a2) s = (a_val a1 s) - (a_val a2 s)
+a_val (Mult a1 a2) s = (a_val a1 s) * (a_val a2 s)
+a_val (Add a1 a2) s = (a_val a1 s) + (a_val a2 s)
+a_val (Sub a1 a2) s = (a_val a1 s) - (a_val a2 s)
 
 b_val :: Bexp -> StateS -> T
 b_val TRUE s = True
@@ -259,31 +239,31 @@ b_val FALSE s = False
 b_val (Neg b) s = case b_val b s of 
                     True -> False
                     False -> True
-b_val (BBinary And b1 b2) s = case ((b_val b1 s), (b_val b2 s)) of 
-                               (True, True) -> True
-                               otherwise -> False
-b_val (RBinary Eq a1 a2) s = if (a_val a1 s) == (a_val a2 s)
-                               then True
-                               else False
-b_val (RBinary Le a1 a2) s = if (a_val a1 s) <= (a_val a2 s)
+b_val (And b1 b2) s = case ((b_val b1 s), (b_val b2 s)) of 
+                       (True, True) -> True
+                       otherwise -> False
+b_val (Eq a1 a2) s = if (a_val a1 s) == (a_val a2 s)
+                       then True
+                       else False
+b_val (Le a1 a2) s = if (a_val a1 s) <= (a_val a2 s)
                         then True
                         else False 
 
 fv_aexp :: Aexp -> [Var]
 fv_aexp (V v) = [v]
 fv_aexp (N n) = []
-fv_aexp (ABinary Mult a1 a2) = (++) (fv_aexp a1) (fv_aexp a2)
-fv_aexp (ABinary Add a1 a2) = (++) (fv_aexp a1) (fv_aexp a2)
-fv_aexp (ABinary Sub a1 a2) = (++) (fv_aexp a1) (fv_aexp a2)
+fv_aexp (Mult a1 a2) = (++) (fv_aexp a1) (fv_aexp a2)
+fv_aexp (Add a1 a2) = (++) (fv_aexp a1) (fv_aexp a2)
+fv_aexp (Sub a1 a2) = (++) (fv_aexp a1) (fv_aexp a2)
 
 subst_aexp :: Aexp -> Var -> Aexp -> Aexp
 subst_aexp (V var) v a2 = if (var == v)
                              then a2
                              else V var
 subst_aexp (N n) v a2 = N n
-subst_aexp (ABinary Mult a11 a12) v a2 = ABinary Mult (subst_aexp a11 v a2) (subst_aexp a12 v a2)
-subst_aexp (ABinary Add a11 a12) v a2 = ABinary Add (subst_aexp a11 v a2) (subst_aexp a12 v a2)
-subst_aexp (ABinary Sub a11 a12) v a2 = ABinary Sub (subst_aexp a11 v a2) (subst_aexp a12 v a2) 
+subst_aexp (Mult a11 a12) v a2 =  Mult (subst_aexp a11 v a2) (subst_aexp a12 v a2)
+subst_aexp (Add a11 a12) v a2  = Add (subst_aexp a11 v a2) (subst_aexp a12 v a2)
+subst_aexp (Sub a11 a12) v a2  = Sub (subst_aexp a11 v a2) (subst_aexp a12 v a2) 
 
 update :: StateS -> Z -> Var -> StateS
 update s i v y = if(v == y) 
@@ -292,6 +272,15 @@ update s i v y = if(v == y)
 
 s' :: StateS
 s' "x" = n_val 5
+
+s :: StateS
+s "x" = n_val 5
+s "y" = n_val 2
+s "z" = n_val 3
+s var = n_val 0
+
+p :: Stm
+p = Comp (Ass "y" (N 1)) (While (Neg (Eq (V "x") (N 1))) (Comp (Ass "y" (Mult (V "y") (V "x"))) (Ass "x" (Sub (V "x") (N 1)))))
 
 cond :: (a -> T, a -> a, a -> a) -> (a -> a)
 cond (c, a1, a2) s = if (c s)
